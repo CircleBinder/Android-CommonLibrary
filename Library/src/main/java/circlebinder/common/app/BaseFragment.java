e0a8b5668bff83b1cad9523afc4b263aebcac0cc
ichigotake <k.wisiiy@gmail.com>
2014-04-22 09:05:54 +0900

    don't open soft input when onResume

--- Library/src/main/java/circlebinder/common/app/BaseFragment.java
+++ Library/src/main/java/circlebinder/common/app/BaseFragment.java
@@ -2,10 +2,16 @@ package circlebinder.common.app;
 
 import android.support.v4.app.Fragment;
 
+import net.ichigotake.common.view.inputmethod.SoftInput;
+
 /**
  * 全てのフラグメントの基底クラス
  */
 abstract public class BaseFragment extends Fragment {
 
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        SoftInput.hide(getView());
+    }
 }
