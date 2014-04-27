f4db8c79445e10f53f06b0e47ac5c48acc91d423
ichigotake <k.wisiiy@gmail.com>
2014-04-27 11:24:02 +0900

    add CircleWebContainer

---
+++ Library/src/main/java/circlebinder/common/circle/CircleWebContainer.java
@@ -0,0 +1,33 @@
+package circlebinder.common.circle;
+
+import android.os.Build;
+import android.webkit.WebView;
+
+public final class CircleWebContainer {
+
+    private final WebView webView;
+
+    public CircleWebContainer(WebView webView) {
+        this.webView = webView;
+    }
+
+    public void initialize() {
+        webView.setWebViewClient(new CircleWebClient());
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            webView.getSettings().setJavaScriptEnabled(true);
+        } else {
+            webView.getSettings().setJavaScriptEnabled(false);
+        }
+    }
+
+    public void loadUrl(String url) {
+        webView.loadUrl(url);
+    }
+
+    public String getCurrentUrl() {
+        return webView.getUrl();
+    }
+}
