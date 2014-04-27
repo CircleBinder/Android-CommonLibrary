22918e50cdaf2e3b36eff130ce4812261bbc9056
ichigotake <k.wisiiy@gmail.com>
2014-04-27 10:32:26 +0900

    add BaseFragment#getRestoreBundle

--- Library/src/main/java/circlebinder/common/app/BaseFragment.java
+++ Library/src/main/java/circlebinder/common/app/BaseFragment.java
@@ -1,13 +1,24 @@
 package circlebinder.common.app;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import net.ichigotake.common.os.RestoreBundle;
 import net.ichigotake.common.view.inputmethod.SoftInput;
 
 /**
  * 全てのフラグメントの基底クラス
  */
 abstract public class BaseFragment extends Fragment {
+
+    private RestoreBundle restoreBundle;
+
+    protected RestoreBundle getRestoreBundle(Bundle savedInstanceState) {
+        if (restoreBundle == null) {
+            restoreBundle = new RestoreBundle(this, savedInstanceState);
+        }
+        return restoreBundle;
+    }
 
     @Override
     public void onResume() {
