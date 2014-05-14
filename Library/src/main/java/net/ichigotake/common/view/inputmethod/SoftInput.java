e54e01db0a35ced36f29eac3b0ce0de9fc160a20
ichigotake <k.wisiiy@gmail.com>
2014-05-14 10:31:13 +0900

    fix can't hide soft input

--- Library/src/main/java/net/ichigotake/common/view/inputmethod/SoftInput.java
+++ Library/src/main/java/net/ichigotake/common/view/inputmethod/SoftInput.java
@@ -1,28 +1,26 @@
 package net.ichigotake.common.view.inputmethod;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 public final class SoftInput {
 
+    private static final String LOG_TAG = SoftInput.class.getSimpleName();
+
+    @SuppressWarnings("ConstantConditions")
     public static void hide(View targetView) {
-        Context context = targetView.getContext();
-        if (context == null) {
-            return ;
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager)
+                    targetView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(
+                    targetView.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS
+            );
+        } catch (NullPointerException e) {
+            Log.e(LOG_TAG, "", e);
         }
 
-        InputMethodManager inputMethodManager = (InputMethodManager)context.getSystemService(
-                Context.INPUT_METHOD_SERVICE
-        );
-
-        if (inputMethodManager == null) {
-            return ;
-        }
-
-        inputMethodManager.hideSoftInputFromWindow(
-                targetView.getWindowToken(),
-                InputMethodManager.HIDE_NOT_ALWAYS
-        );
     }
 }
