eb2ba03d1fca9ae069a9ff71ca6005f4b1ac0851
ichigotake <k.wisiiy@gmail.com>
2014-05-28 20:56:52 +0900

    add ChecklistPopupSelector

---
+++ Library/src/main/java/circlebinder/common/checklist/ChecklistPopupSelector.java
@@ -0,0 +1,48 @@
+package circlebinder.common.checklist;
+
+import android.content.Context;
+import android.support.v7.internal.widget.ListPopupWindow;
+import android.view.View;
+
+import net.ichigotake.common.widget.OnItemClickEventListener;
+import net.ichigotake.common.widget.OnItemClickListener;
+
+public final class ChecklistPopupSelector {
+
+    private final Context context;
+    private final View anchor;
+    private final OnItemClickListener<ChecklistColor> listener;
+    private final ListPopupWindow popupWindow;
+
+    public ChecklistPopupSelector(Context context, View anchor) {
+        this.context = context;
+        this.anchor = anchor;
+        this.popupWindow = new ListPopupWindow(context);
+        this.listener = new OnItemClickListener<ChecklistColor>();
+    }
+
+    public void setOnItemClickListener(OnItemClickEventListener<ChecklistColor> listener) {
+        this.listener.addOnItemClickEventListener(listener);
+    }
+
+    public void show() {
+        ChecklistSelectorAdapter adapter = new ChecklistSelectorAdapter(
+                context, new ChecklistSelectorBinder()
+        );
+        adapter.add(ChecklistColor.NONE);
+        for (ChecklistColor item : ChecklistColor.checklists()) {
+            adapter.add(item);
+        }
+        popupWindow.setAdapter(adapter);
+        popupWindow.setAnchorView(anchor);
+        popupWindow.setOnItemClickListener(listener);
+        popupWindow.setModal(true);
+        //TODO: 決め打ちやめたい
+        popupWindow.setWidth(800);
+        popupWindow.show();
+    }
+
+    public void dismiss() {
+        popupWindow.dismiss();
+    }
+}
