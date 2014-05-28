0d631ea9450aa3021ff78dce2f7953f26c6aeca4
ichigotake <k.wisiiy@gmail.com>
2014-05-28 20:45:12 +0900

    OnClickListener on ViewBinder is excessive operation

--- Library/src/main/java/circlebinder/common/circle/CircleViewBinder.java
+++ Library/src/main/java/circlebinder/common/circle/CircleViewBinder.java
@@ -11,12 +11,6 @@ import circlebinder.common.event.Circle;
 
 public final class CircleViewBinder implements ViewBinder<Circle, CircleViewHolder> {
 
-    private final CircleOnClickListener listener;
-
-    public CircleViewBinder(CircleOnClickListener listener) {
-        this.listener = listener;
-    }
-
     @Override
     public CircleViewHolder generateTag(int position, Circle item, View convertView) {
         return new CircleViewHolder(convertView);
@@ -32,10 +26,8 @@ public final class CircleViewBinder implements ViewBinder<Circle, CircleViewHold
         int backgroundColor = item.getChecklistColor().getColor() + 0xcc000000;
 
         tag.getContainer().setBackgroundColor(backgroundColor);
-        tag.getContainer().setOnClickListener(new OnCircleClickListener(listener, item, position));
         tag.getCircleName().setText(item.getName());
         tag.getPenName().setText(item.getPenName());
-        tag.getCircleCut().setOnClickListener(new OnCircleCutClickListener(listener, item, position));
         //TODO: 外部リンクを複数設置出来るようにする。今は1つで決め打ち
         if (item.getLinks().isEmpty()) {
             tag.getLinks().setVisibility(View.GONE);
@@ -44,9 +36,6 @@ public final class CircleViewBinder implements ViewBinder<Circle, CircleViewHold
         }
 
         tag.getCircleCut().setVisibility(View.GONE);
-        tag.getCheckBox().setChecked(item.isChecked());
-        listener.onCheckBoxClick(item, tag, item.isChecked());
-        tag.getSpace().setOnClickListener(new OnCircleCheckBoxClickListener(item, tag, listener));
         tag.getSpace().setText(
                 String.format("%s\n%02d%s",
                         item.getSpace().getBlockName(), item.getSpace().getNo(), item.getSpace().getNoSub())
@@ -54,39 +43,4 @@ public final class CircleViewBinder implements ViewBinder<Circle, CircleViewHold
         tag.getGenre().setText(item.getGenre().getName());
     }
 
-    private static class OnCircleClickListener implements View.OnClickListener {
-
-        private final CircleOnClickListener listener;
-        private final Circle circle;
-        private final int position;
-
-        OnCircleClickListener(CircleOnClickListener listener, Circle circle, int position) {
-            this.listener = listener;
-            this.circle = circle;
-            this.position = position;
-        }
-
-        @Override
-        public void onClick(View v) {
-            listener.onItemClick(circle, position);
-        }
-    }
-
-    private final static class OnCircleCutClickListener implements View.OnClickListener {
-
-        private final CircleOnClickListener listener;
-        private final Circle circle;
-        private final int position;
-
-        private OnCircleCutClickListener(CircleOnClickListener listener, Circle circle, int position) {
-            this.listener = listener;
-            this.circle = circle;
-            this.position = position;
-        }
-
-        @Override
-        public void onClick(View v) {
-            listener.onCircleCutClick(circle, position);
-        }
-    }
 }
