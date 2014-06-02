de147ba471186f098c11ca5dc69cd5d9ba06465d
ichigotake <k.wisiiy@gmail.com>
2014-06-02 10:51:58 +0900

    collect Activity navigation implementation to ActivityUtils

--- Library/src/main/java/net/ichigotake/common/app/ActivityNavigation.java
+++ Library/src/main/java/net/ichigotake/common/app/ActivityNavigation.java
@@ -1,12 +1,17 @@
-package circlebinder.common.app;
+package net.ichigotake.common.app;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
-public final class UpNavigation {
+public final class ActivityNavigation {
+
+    public static boolean hasParentActivity(ActionBarActivity activity) {
+        return activity != null && activity.getSupportParentActivityIntent() != null;
+    }
 
     public static boolean onOptionsItemSelected(Activity currentActivity, MenuItem item) {
         if (item.getItemId() != android.R.id.home) {
