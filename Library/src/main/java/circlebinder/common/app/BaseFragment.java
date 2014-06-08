a8af3f41a980e0497a648d74aa250632714aa9c3
ichigotake <k.wisiiy@gmail.com>
2014-06-08 11:21:30 +0900

    RestoreBundle as delegate class in BaseFragment

--- Library/src/main/java/circlebinder/common/app/BaseFragment.java
+++ Library/src/main/java/circlebinder/common/app/BaseFragment.java
@@ -1,25 +1,14 @@
 package circlebinder.common.app;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 
-import net.ichigotake.common.os.RestoreBundle;
 import net.ichigotake.common.view.inputmethod.SoftInput;
 
 /**
  * 全てのフラグメントの基底クラス
  */
 abstract public class BaseFragment extends Fragment {
-
-    private RestoreBundle restoreBundle;
-
-    protected RestoreBundle getRestoreBundle(Bundle savedInstanceState) {
-        if (restoreBundle == null) {
-            restoreBundle = new RestoreBundle(this, savedInstanceState);
-        }
-        return restoreBundle;
-    }
 
     protected ActionBarActivity getSupportActivity() {
         return (ActionBarActivity)getActivity();
