7076fe3dabdb9d8617310d0579763899ccda06ca
ichigotake <k.wisiiy@gmail.com>
2014-06-14 16:46:52 +0900

    fix crash when use SqlQueryBuilder#build

--- Library/src/main/java/net/ichigotake/common/database/SqlQueryBuilder.java
+++ Library/src/main/java/net/ichigotake/common/database/SqlQueryBuilder.java
@@ -55,7 +55,7 @@ public final class SqlQueryBuilder {
         }
 
         public Object[] getArguments() {
-            return values.toArray();
+            return values.toArray(new String[values.size()]);
         }
 
         @Override
