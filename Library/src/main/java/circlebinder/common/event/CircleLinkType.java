462cfc82cffdc8245b7e82a0deac105e30ed4754
ichigotake <k.wisiiy@gmail.com>
2014-07-01 10:53:16 +0900

    Remove unused interfaces

--- Library/src/main/java/circlebinder/common/event/CircleLinkType.java
+++ Library/src/main/java/circlebinder/common/event/CircleLinkType.java
@@ -4,20 +4,14 @@ import circlebinder.common.R;
 
 public enum CircleLinkType {
 
-    PIXIV(R.string.circlebinder_circle_link_pixiv, R.id.circlebinder_menu_circle_links_pixiv),
-    HOMEPAGE(R.string.circlebinder_circle_link_homepage, R.id.circlebinder_menu_circle_links_homepage),
+    PIXIV(R.string.circlebinder_circle_link_pixiv),
+    HOMEPAGE(R.string.circlebinder_circle_link_homepage),
     ;
 
     private final int name;
-    private final int menuItemId;
 
-    private CircleLinkType(int name, int menuItemId) {
+    private CircleLinkType(int name) {
         this.name = name;
-        this.menuItemId = menuItemId;
-    }
-
-    public int getMenuItemId() {
-        return menuItemId;
     }
 
     public int getNameResource() {
